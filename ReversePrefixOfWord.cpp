a4c4f74f85b208269616683e3414ecc8fea9b027
Shantanu9799 <cshantanu712@gmail.com>
2024-05-01 21:42:14 +0530

    potd

--- ReversePrefixOfWord.cpp
+++ ReversePrefixOfWord.cpp
@@ -14,3 +14,26 @@ public:
         return word;
     }
 };
+
+
+
+
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int idx = -1;
+        for(int i = 0; i < word.size(); ++i) {
+            if(word[i] == ch) {
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) return word;
+        string revPref = word.substr(0, idx+1);
+        reverse(revPref.begin(), revPref.end());
+        // cout << revPref << endl;
+        revPref += word.substr(idx+1, word.size() - idx); 
+        return revPref;
+    }
+};
