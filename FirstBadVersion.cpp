346cb9170f3f1a813636648931571f17341bc9c1
Shantanu Chatterjee <85756136+Shantanu9799@users.noreply.github.com>
2026-01-06 12:51:40 +0530

    Add binary search solution for first bad version
    Implement binary search to find the first bad version.

---
+++ FirstBadVersion.cpp
@@ -0,0 +1,29 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1, high = n;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(isBadVersion(mid)) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+};
+
+/*
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for(int i = 1; i <= n; ++i) {
+            if(isBadVersion(i)) return i;
+        }
+        return 0;
+    }
+};
+
+*/
